e738ef60daafa237b69bab5b1e9fc890ec5ee5e6
cyber-dev0x <269818201+cyber-dev0x@users.noreply.github.com>
2026-03-31 18:18:05 +0300

    Add production GitHub project link

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,3 +1,4 @@
 export const SITE_LINKS = {
-  github: process.env.NEXT_PUBLIC_GITHUB_URL ?? "https://github.com/",
+  github:
+    process.env.NEXT_PUBLIC_GITHUB_URL ?? "https://github.com/cyber-dev0x/quantum-center",
 };
